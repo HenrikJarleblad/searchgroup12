af7ff4c47e0757d45467767801b58ab85f8f4523
KardaChalak <karda.chalak@gmail.com>
2018-05-09 15:28:37 +0200

    django framework init

--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def answer(request):
+
+  """
+  API endpoint that retunrs answer to a question
+  """
+
+  print("question")
+
+  return Response({"answer": "first"})
